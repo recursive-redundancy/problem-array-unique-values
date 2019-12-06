6fdb1ce7ff56f2e56738a900aae780da2489679c
recursive-redundancy <danielmorgan.dev@gmail.com>
2019-12-06 16:52:39 -0600

    added unit test script

---
+++ test.js
@@ -0,0 +1,38 @@
+/**
+ * Unit testing script
+ */
+
+const { unqVals_Set } = require('./unique-values');
+
+/**
+ * set of arrays to test
+ */
+let tests = [
+  {
+    arr: [9, 3, 3, 0, 1, 4, 1, 3, 6, 6],
+    soln: 6
+  },
+  {
+    arr: [1, 2, 1, 2, 3],
+    soln: 3
+  },
+  {
+    arr: [],
+    soln: 0
+  }
+];
+
+/** run solnLinearTime on each test array */
+tests.forEach((test) => {
+  console.log('running test on: ' + test.arr);
+  let result = unqVals_Set(test.arr);
+  console.log('expected=' + test.soln);
+  console.log('result=' + result);
+  if (test.soln == result) {
+    console.log('***SUCCESS***');
+  }
+  else {
+    console.log('***FAILURE***');
+  }
+  console.log('-----------------')
+})
