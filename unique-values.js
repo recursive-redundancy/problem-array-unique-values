d4f64b2b8a87285e11d8c5c9c557d1b3dec183bc
recursive-redundancy <danielmorgan.dev@gmail.com>
2019-12-06 19:25:59 -0600

    added second method for solving

--- unique-values.js
+++ unique-values.js
@@ -9,4 +9,18 @@ function unqVals_Set(arr) {
   return unq.size;
 }
 
-module.exports = { unqVals_Set };
+/**
+ * Finds the number of unique values in array using
+ * JavaScript's built-in Map object
+ * @param {Array} arr - Array to find unique values from
+ * @returns {Number} - An integer value representing number of unique values
+ */
+function unqVals_Map(arr) {
+  let unq = new Map();
+  arr.forEach(element => {
+    unq.set(element, null);
+  });
+  return unq.size;
+}
+
+module.exports = { unqVals_Set, unqVals_Map };
